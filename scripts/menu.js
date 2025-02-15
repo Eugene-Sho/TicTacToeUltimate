8d2424fb0f44e113d08ea5b04b93a7c6f7a1728d
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-02-15 22:04:56 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,5 @@
     document.addEventListener("click", () => {
         const audio = document.getElementById("bg-music");
-        if(audio.volume == 0.1)return;
         audio.volume = 0.1;
         audio.play().catch(error => console.log("Автовоспроизведение заблокировано:", error));
     });
