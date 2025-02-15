1a37047a99cd48ca5b317e51db52fccfa4e7dbb4
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-02-15 21:33:46 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,9 @@
-const tg = window.Telegram.WebApp;
-    tg.setHeaderColor("#1a1a1a");
-    tg.isClosingConfirmationEnabled = true;
-    tg.isVerticalSwipesEnabled = false;
-    tg.expand();
-
     document.addEventListener("click", () => {
         const audio = document.getElementById("bg-music");
         audio.volume = 0.1;
         audio.play().catch(error => console.log("Автовоспроизведение заблокировано:", error));
     });
+    function onPageLoad()
+    {
+        document.getElementById("bg-music").volume = 0.1;
+    }
