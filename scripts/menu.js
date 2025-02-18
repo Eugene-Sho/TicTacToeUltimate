d1de1d162fd87c7275dbf58472409f4cc63b975d
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-02-18 23:53:36 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,25 +1,17 @@
-    document.addEventListener("click", () => {
-        const audio = document.getElementById("bg-music");
-        audio.play().catch(error => console.log("Автовоспроизведение заблокировано:", error));
-    });
-    function onPageLoad()
-    {
-        document.getElementById("bg-music").volume = 0.1;
-    }
-    function add(btn)
-    {
-        document.getElementById(btn).classList.add("active");
-    }
-    function rem(btn)
-    {
-        document.getElementById(btn).classList.remove("active");
-    }
-    function TTTmenu(isEnter)
-    {
-        document.getElementById("btn1").style.display = isEnter ? "none" : "unset";
-        document.getElementById("btn2").style.display = isEnter ? "none" : "unset";
-        document.getElementById("btn3").style.display = !isEnter ? "none" : "unset";
-        document.getElementById("btn4").style.display = !isEnter ? "none" : "unset";
-        document.getElementById("returnBtn").style.display = !isEnter ? "none" : "unset";
-        document.getElementById("logo").innerText = isEnter ? "Ultimate TicTacToe" : "EUGENE's MEGA ULTRA SUPER DUPER GAMES";
-    }
+const buttonsToHide = ["btn1", "btn2"];
+const buttonsToShow = ["btn3", "btn4", "returnBtn"];
+
+function add(btn){ document.getElementById(btn).classList.add("active"); }
+function rem(btn){ document.getElementById(btn).classList.remove("active"); }
+
+document.addEventListener("click", () => {
+    const audio = document.getElementById("bg-music");
+    audio.play().catch(error => console.log("Заблокировано:", error));
+});
+function TTTmenu(isEnter)
+{
+    buttonsToHide.forEach(buttonId => { document.getElementById(buttonId).style.display = isEnter ? "none" : "unset"; });
+    buttonsToShow.forEach(buttonId => { document.getElementById(buttonId).style.display = isEnter ? "unset" : "none"; });
+
+    document.getElementById("logo").innerText = isEnter ? "Ultimate TicTacToe" : "EUGENE's MEGA ULTRA SUPER DUPER GAMES";
+}
