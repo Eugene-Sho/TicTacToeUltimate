16b62e394b89a9faa79168247a8439dd6277b052
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-02-18 15:09:52 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -15,3 +15,12 @@
     {
         document.getElementById(btn).classList.remove("active");
     }
+    function TTTmenu(isEnter)
+    {
+        document.getElementById("btn1").style.display = isEnter ? "none" : "unset";
+        document.getElementById("btn2").style.display = isEnter ? "none" : "unset";
+        document.getElementById("btn3").style.display = !isEnter ? "none" : "unset";
+        document.getElementById("btn4").style.display = !isEnter ? "none" : "unset";
+        document.getElementById("returnBtn").style.display = !isEnter ? "none" : "unset";
+        document.getElementById("logo").innerText = isEnter ? "Ultimate TicTacToe" : "EUGENE's MEGA ULTRA SUPER DUPER GAMES";
+    }
