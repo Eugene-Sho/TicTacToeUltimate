bb372f7f9f821c01923f8a429cbbfa3fff066b80
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-02-18 11:37:42 +0100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -7,3 +7,11 @@
     {
         document.getElementById("bg-music").volume = 0.1;
     }
+    function add(btn)
+    {
+        document.getElementById(btn).classList.add("active");
+    }
+    function rem(btn)
+    {
+        document.getElementById(btn).classList.remove("active");
+    }
