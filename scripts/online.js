2a1cb493707da53ef99a7f3d3cdf8ba89f0d0211
Eugene-Sho <121756034+Eugene-Sho@users.noreply.github.com>
2025-03-29 18:20:22 +0100

    Update online.js

--- scripts/online.js
+++ scripts/online.js
@@ -6,7 +6,7 @@ if (!user) {
 */
 var interval1 = null;
 var interval2 = null;
-const randomNumber = Math.floor(100000000 + Math.random() * 900000000);
+var randomNumber = Math.floor(100000000 + Math.random() * 900000000);
 
 function onPageLoad()
 {
